cc87adcb89eb0deebf42cffda50584cc1ce47a5b
Daniel Mansilla <135163986+danimansilla23@users.noreply.github.com>
2024-10-09 10:59:52 -0300

    Se habilito CORS, para permitir interacciones
    before(): Este método se utiliza para interceptar todas las solicitudes antes de que se procesen. Aquí es donde puedes agregar encabezados CORS.
    Access-Control-Allow-Origin: Este encabezado indica a los navegadores que se permite el acceso desde cualquier origen (en este caso, el asterisco * significa que se permite cualquier origen). Si solo deseas permitir solicitudes desde http://localhost, puedes cambiarlo por http://localhost.
    Access-Control-Allow-Methods: Especifica qué métodos HTTP son permitidos para acceder a la API.
    Access-Control-Allow-Headers: Indica qué encabezados pueden ser utilizados en las solicitudes.

---
+++ Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.empresa;
+
+import static spark.Spark.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Sql2oDAO.getSql2o();
+            System.out.println("Conexión exitosa a la base de datos");
+        } catch (Exception e) {
+            System.err.println("No se pudo conectar a la base de datos: " +
+                    e.getMessage());
+        }
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*"); // Permitir todas las fuentes
+            response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Métodos permitidos
+            response.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Encabezados permitidos
+        });
+        
+        get("/getMateriasPrimas",MpController.getAll);
+        get("/getMateriaPrima/:id", MpController.getById);
+        delete("/deleteMateriaPrima/:id", MpController.delete);
+        put("/updateMateriaPrima/:id", MpController.update);
+        post("/addMateriaPrima", MpController.add);
+
+    }
+}
